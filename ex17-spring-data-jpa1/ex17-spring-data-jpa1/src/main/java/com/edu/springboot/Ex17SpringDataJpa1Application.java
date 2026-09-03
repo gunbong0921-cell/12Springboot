56032c7ac7977726f46352e803a8a8b82789c5e6
gunbong0921-cell <gunbong0921@gmail.com>
2026-09-03 12:59:05 +0900

    1-1

---
+++ ex17-spring-data-jpa1/ex17-spring-data-jpa1/src/main/java/com/edu/springboot/Ex17SpringDataJpa1Application.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex17SpringDataJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex17SpringDataJpa1Application.class, args);
+	}
+
+}
---
+++ ex17-spring-data-jpa1/ex17-spring-data-jpa1/src/test/java/com/edu/springboot/Ex17SpringDataJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex17SpringDataJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
